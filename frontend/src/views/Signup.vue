a366f3c12467c369153dc3fa6973fdf7dd05f325
Magone <magoniiites@gmail.com>
2021-06-26 12:11:06 +0200

    Regex page login

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -6,13 +6,13 @@
     <div class="container p-5">
       <p class="h4 mb-4 text-center">{{title}}</p>
       <form class="jumbotron py-6" @submit.prevent>
-        
         <div class="required mb-2 text-center">Les champs requis *</div>
         <div class="signup-inputs">
-          <input class="form-control mb-4" v-model="firstName" ref="firstName" type="text" placeholder="Prénom *" title="Saisissez votre prénom" required>
-          <input class="form-control mb-4" v-model="lastName" ref="lastName" type="text" placeholder="Nom *"  title="Saisissez votre nom de famille" required>
-          <input class="form-control mb-4" v-model="email" ref="email" type="email" placeholder="E-mail *" title="Saisissez votre adresse e-mail" required>
-          <input class="form-control mb-4" v-model="password" ref="password" type="password" placeholder="Mot de passe *" title="Saisissez un mot de passe" required>
+          <div class="validity-msg">{{ messageValidity }}</div>
+          <input class="form-control mb-4" v-model="firstName" v-on:change="firstNameValid" ref="firstName" type="text" placeholder="Prénom *" title="Saisissez votre prénom" required>
+          <input class="form-control mb-4" v-model="lastName" v-on:change="lastNameValid" ref="lastName" type="text" placeholder="Nom *"  title="Saisissez votre nom de famille" required>
+          <input class="form-control mb-4" v-model="email" v-on:change="emailValid" ref="email" type="email" placeholder="E-mail *" title="Saisissez votre adresse e-mail" required>
+          <input class="form-control mb-4" v-model="password" v-on:change="passwordValid" ref="password" type="password" placeholder="Mot de passe *" title="Saisissez un mot de passe" required>
           <input class="form-control mb-4" v-model="confirmPassword" ref="confirmPassword" type="password" placeholder="Confirmer mot de passe *" title="Confirmez votre mot de passe" required>
         </div>
         <div class="error-msg">{{ message }}</div>
@@ -41,20 +41,30 @@ export default {
       email: "",
       password : "",
       confirmPassword: "",
+      messageValidity: "",
       message: "",
+      validFirstName: false,
+      validLastName: false,
+      validEmail: false,
+      validPassword: false
     }
   },
 
   methods: {
+  
     signup() {
       const firstName = this.firstName;
       const lastName = this.lastName;
       const email = this.email;
       const password = this.password;
       const confirmPassword = this.confirmPassword;
+      const validFirstName = this.validFirstName;
+      const validLastName = this.validLastName;
+      const validEmail = this.validEmail;
+      const validPassword = this.validPassword;
       console.log(confirmPassword);
 
-      if(password === confirmPassword){
+      if(validFirstName === true && validLastName === true && validEmail == true && validPassword == true && password === confirmPassword && password.length >= 8 && firstName.length >= 3 && lastName.length >= 3){
         axios.post("http://localhost:3000/api/user/signup", {
           firstName,
           lastName,
@@ -62,20 +72,65 @@ export default {
           password
         })
         .then((res) => {
-          console.log("Message backend :", res.data.message);
+          console.log(res.data.message);
           this.$router.push('/user/login');
         })
         .catch((e) => {
           if (e.response.status === 400) {
-            this.message = "Le format de mot de passe n'est pas correct ! Le mot de passe doit contenir au moins 8 caractères, au moins 1 majuscule, au moins 1 minuscule, au moins 1 chiffre et doit être sans les espaces !";
+            this.message = "Veuillez corriger le formulaire !";
           }
           if (e.response.status === 500) {
             this.message = "E-mail d'utilisateur existe déjà !";
-            //this.message = error.res.data.error;
           }
         })
       } else {
-        this.message = "Veuillez confirmer le mot de passe !";
+        this.message = "Veuillez corriger les champs du formulaire !";
+      }
+    },
+
+    firstNameValid() {
+      const firstNameRe = /^[A-Za-zÀ-ÿ ]+$/;
+
+      if (firstNameRe.test(this.firstName)) {
+        this.validFirstName = true,
+        this.messageValidity = ""
+      } else {
+        this.validFirstName = false;
+        this.messageValidity = "Le prénom doit être au mons 3 caractères long et contenir que de lettres et espaces";
+      }
+    },
+
+    lastNameValid() {
+      const lastNameRe = /^[A-Za-zÀ-ÿ ]+$/;
+
+      if (lastNameRe.test(this.lastName)) {
+        this.validLastName = true,
+        this.messageValidity = ""
+      } else {
+        this.validLastName = false;
+        this.messageValidity = "Le nom doit être au mons 3 caractères long et contenir que de lettres et espaces";
+      }
+    },
+
+      emailValid() {
+      const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (emailRe.test(this.email)) {
+        this.validEmail = true,
+        this.messageValidity = ""
+      } else {
+        this.validEmail = false;
+        this.messageValidity = "Le format d'e-mail n'est pas valide, Ex: mail@mail.com";
+      }
+    },
+
+    passwordValid() {
+      const passwordRe = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+      if (passwordRe.test(this.password)) {
+        this.validPassword = true,
+        this.messageValidity = ""
+      } else {
+        this.validPassword = false;
+        this.messageValidity = "Le format de mot de passe n'est pas correct ! Le mot de passe doit contenir au moins 8 caractères, au moins 1 majuscule, au moins 1 minuscule, au moins 1 chiffre et doit être sans les espaces !";
       }
     }
   }
